c833451b21a55fec43f820eb4b671a987aaed5f2
rio45ka <rio.aska35@gmail.com>
2020-07-09 17:35:02 +0700

    handle error fecth data home, implement onclick in category item

--- app/src/main/java/com/auroraministudio/sehatqassignment/feature/home/presentation/view/HomeFragment.kt
+++ app/src/main/java/com/auroraministudio/sehatqassignment/feature/home/presentation/view/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -62,6 +63,11 @@ class HomeFragment : Fragment() {
 
         homeViewModel.listCategory.observe(viewLifecycleOwner, Observer {
             adapterCategory.categories = it
+        })
+
+        homeViewModel.errorFetchData.observe(viewLifecycleOwner, Observer {
+            if (it != null) Toast.makeText(context, "Couldn't refresh, $it", Toast.LENGTH_SHORT)
+                .show()
         })
     }
 
